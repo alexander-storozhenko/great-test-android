362945cbbd4c5f4a25f0b370e20a2b074693e030
Denis <d_prokopenko@rambler.ru>
2020-12-13 21:44:22 +0700

    kek

--- Components/reportMailRoom/reportMailRoom.js
+++ Components/reportMailRoom/reportMailRoom.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Button, View, Text, StyleSheet } from "react-native";
-import { Link } from 'react-router-native';
-import { lightColor, navHeight, secondaryColor } from '../StyleConstants';
-import TestTwo from '../svg/TestTwo';
-import RecommendsNav from '../svg/RecommendsNav';
-import SearchNav from '../svg/SearchNav';
-import ProfileNav from '../svg/ProfileNav'
-import { TouchableNativeFeedback } from 'react-native-gesture-handler';
-import { NavigationActions } from 'react-navigation';
-import * as NavigationService from '../../lib/NavigationService'
-
 
 class NavBar extends Component {
-
+//
     constructor(props){
         super(props)
     }
+
     _navigate(route){
-        NavigationService.navigate(route, { userName: 'Lucy' });
+        NavigationService.navigate(route);
     }
 
     render() {
         return (
             <View style={{ width: '100%', height: navHeight, justifyContent: 'flex-start', flexDirection: 'row', backgroundColor: lightColor }}>
-
-{/* NavigationActions.navigate({routeName:'Main'}) */}
                 <View style={{ width: "33.33333%" }}>
                     <TouchableNativeFeedback onPress={()=> this._navigate('Main')} style={styles.nav_item}>
                         <View style={styles.nav_item}>
@@ -52,28 +41,7 @@ class NavBar extends Component {
                         </View>
                     </TouchableNativeFeedback>
                 </View>
-                {/* <Link to="/test"><Text>KEKK</Text></Link> */}
-                {/* <MainRoom_Card title="New 2021 year!" backgroundColor={{ first: "#1f6eed", second: "#1fed75" }} */}
-                {/* subTitle="Who you from new year fairy tail?" /> */}
-
-                {/* <MainRoom_Card /> */}
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    nav_item: {
-        height: '100%',
-        // width: '100%',
-        backgroundColor: lightColor,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    nav_item_text: {
-        fontSize: 11,
-
-     }
-})
-
-export default NavBar;
